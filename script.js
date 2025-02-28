d14953e2280d3833359904bd5eb40e398fae7581
shikha1421 <30616325+shikha1421@users.noreply.github.com>
2025-02-27 22:57:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Countdown Timer
-const countdownDate = new Date("March 15, 2025 00:00:00").getTime();
-
-const countdownFunction = setInterval(function() {
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
-
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown-timer").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(countdownFunction);
-        document.getElementById("countdown-timer").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
 // Slideshow: Show one image at a time and change every 5 seconds
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
